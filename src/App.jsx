fdbe3e81487b4897b7c92ce0bcb5cd7edd51751c
Sidath Samarasinghe <sidaath@gmail.com>
2023-09-20 23:20:19 +0200

    fetch users in custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [users, setUsers] = useState();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [{ users, loading, error }, setUsers] = useFetchUsers();
 
   useEffect(() => {
     const controller = new AbortController();
+    setUsers(controller);
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>Users</h1>
+      {error && <h2>Error fetching users</h2>}
+      {loading && <h2>Loading . . . </h2>}
+      {users.length > 0 &&
+        loading !== true &&
+        users.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+    </>
+  );
+}
+
+function useFetchUsers() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  function fetchUsers(x) {
     setLoading(true);
     setError(false);
+
     fetch("https://jsonplaceholder.typicode.com/users", {
-      signal: controller.signal,
+      signal: x.signal,
     })
       .then((res) => {
-        console.log("resolved");
-        console.log(res);
-        console.log("returning .json()");
-        return res.json();
+        if (res.ok) {
+          console.log("resolved");
+          console.log(res);
+          console.log("returning .json()");
+          return res.json();
+        }
+        return Promise.reject(res);
       })
       .then((data) => {
         console.log("data resolved, setting users");
@@ -30,29 +59,14 @@ function App() {
           return;
         } else {
           console.error(e);
-          setLoading(false);
+          console.log("bad error");
           setError(true);
+          setLoading(false);
         }
       });
+  }
 
-    console.log("outaside the fetch");
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
-  return (
-    <>
-      <h1>Users</h1>
-      {error && <h1>Error</h1>}
-      {loading && <h2>Loading ....</h2>}
-      {!loading &&
-        users.map((user) => {
-          return <li key={user.id}>{user.name}</li>;
-        })}
-    </>
-  );
+  return [{ users, loading, error }, fetchUsers];
 }
 
 export default App;
